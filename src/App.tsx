4e9644ede0b4420d73cd09474b95a62e2f8523f4
Nikolay-Gruzd <gruzd.kolyan@gmail.com>
2026-05-22 20:50:05 +0800

    Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import './App.css';
-import {Header} from "./site/Header.tsx";
-import {Body} from "./site/Body.tsx";
-import {Footer} from "./site/Footer.tsx";
 
 export function App() {
     return (
         <div className='App'>
-            <Header title={'Header props'} />
-            <Body title={'Body props'} />
-            <Footer title={'Footer props'} />
+
         </div>
     );
 }
