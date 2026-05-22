9348469f38446ee028963372afa7ab44323c8691
Nikolay-Gruzd <gruzd.kolyan@gmail.com>
2026-05-22 22:46:42 +0800

    MicroTask_02_sprint-1_2
    Input: types, props, functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
 import './App.css';
+import {useState} from "react";
+import {Input} from "./components/Input.tsx";
+import {Button} from "./components/Button.tsx";
 
 export function App() {
+    // Data //
+    let [message, setMessage] = useState([
+        {message: 'message1'},
+        {message: 'message2'},
+        {message: 'message3'},
+    ]);
+
+    let [title, setTitle] = useState('');
+
+    // Functions //
+    const addMessage = (title: string) => {
+        setMessage([
+            {message: title},
+            ...message,
+        ])
+    }
+
+    // Handlers //
+    const callBackButtonHandler = () => {
+        addMessage(title)
+        setTitle('')
+    }
+
     return (
         <div className='App'>
-
+            <Input title={title} setTitle={setTitle}/>
+            <Button name={'+'} callBack={callBackButtonHandler}/>
+            {message.map((el, index) => {
+                return (
+                    <div key={index}>{el.message}</div>
+                )
+            } )}
         </div>
     );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+
+type ButtonType = {
+    name: string,
+    callBack: () => void
+}
+
+export const Button = (props: ButtonType) => {
+
+    const onClickButtonHandler = () => {
+        props.callBack()
+    }
+
+    return (
+        <button onClick={onClickButtonHandler}>{props.name}</button>
+    )
+}
