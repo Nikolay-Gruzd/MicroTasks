a372b8d37fd6328c11d9691437bfb42ee1b80381
Nikolay-Gruzd <gruzd.kolyan@gmail.com>
2026-08-21 00:01:37 +0800

    MicroTask_sprint-3_1
    React router dom - part 1
    
    Error404, Navigate

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import styles from "./components/Site.module.css";
 import {PageOne} from "./components/pages/PageOne.tsx";
 import {PageTwo} from "./components/pages/PageTwo.tsx";
 import {PageThree} from "./components/pages/PageThree.tsx";
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import {Error404} from "./components/pages/Error404.tsx";
 
 export const App = () => {
@@ -14,12 +14,13 @@ export const App = () => {
                     Здесь будет навигация
                 </div>
                 <div className={styles.content}>
-                    Здесь будут кроссовки
                     <Routes>
+                        <Route path='/' element={<Navigate to={'/page1'} />}/>
                         <Route path="/page1" element={<PageOne />}/>
                         <Route path="/page2" element={<PageTwo/>}/>
                         <Route path="/page3" element={<PageThree/>}/>
-                        <Route path="/*" element={<Error404/>}/>
+                        <Route path="/error404" element={<Error404/>}/>
+                        <Route path="/*" element={<Navigate to={'/error404'}/>}/>
                     </Routes>
                 </div>
             </div>
