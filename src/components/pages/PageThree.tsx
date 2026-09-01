fe3dca393ffce7b9c697bae73a12a71bccaff3ff
Nikolay-Gruzd <gruzd.kolyan@gmail.com>
2026-09-02 00:11:00 +0800

    MicroTask_sprint-3_1
    React router dom - part 2
    
    NavLink

--- src/components/pages/PageThree.tsx
+++ src/components/pages/PageThree.tsx
@@ -3,7 +3,7 @@ export const PageThree = () => {
         <div>
             <h2> ABIBAS</h2>
             <p>
-                What is Lorem Ipsum?
+                What is Lorem Ipsum? 3
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
                 dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                 It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
